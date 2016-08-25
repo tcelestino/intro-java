4ee4196410008a405839fae4671a2967145b92c2
Tiago Celestino <tcelestino@gmail.com>
2016-08-24 23:17:44 -0300

    create new files

---
+++ MyPersonalInfo.java
@@ -0,0 +1,13 @@
+class MyPersonalInfo {
+  public static void main(String[] args) {
+    String myName = "Tiago Celestino";
+    int age = 29;
+    double weight = 78.9; //é verdade
+    boolean isBrazilian = true;
+
+    System.out.println(myName);
+    System.out.println(age);
+    System.out.println(weight);
+    System.out.println(isBrazilian);
+  }
+}
